c36269a8df0b57017f82896cf8dc43b5572c52fa
SiddDevZ <epic.siddarth@gmail.com>
2024-06-28 23:29:59 +0530

    Added more Sections

--- src/Frontend/Pages/Landing/Landing.jsx
+++ src/Frontend/Pages/Landing/Landing.jsx
@@ -9,12 +9,10 @@ const Landing = () => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const threshold = 300;
+      const navElement = document.querySelector('nav');
 
-      if (scrollPosition > threshold) {
-        document.body.classList.add('scrolled');
-      } else {
-        document.body.classList.remove('scrolled');
-      }
+      const blurAmount = Math.min((scrollPosition / threshold) * 10, 10);
+      navElement.style.backdropFilter = `blur(${blurAmount}px)`;
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -26,10 +24,10 @@ const Landing = () => {
 
   return (
     <>
-      <div className="h-screen w-full bg-grid-white/[0.06] relative flex">
+      <div className="h-max w-full bg-grid-white/[0.06] relative flex">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-yell [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)]"></div>
-        <div className='z-20'>
+        <div className='z-20 animate-in'>
 
           <nav className='flex w-full sm:px-20 md:px-24 lg:px-28 nav items-center justify-between'>
             <div className='flex items-center justify-between w-[127px]'>
@@ -52,22 +50,93 @@ const Landing = () => {
             <img src="skull.png" alt="" className='ae e1'/>
             <img src="sunglasses.png" alt="" className='ae e2'/>
             <img src="laugh.png" alt="" className='ae e3'/>
+            <img src="heart.png" alt="" className='ae e4'/>
+            <img src="eyes.png" alt="" className='ae e5'/>
           </div>
 
-          <main className='flex w-[100vw] z-500 h-[80vh] items-center mt-[10vh] justify-between sm:px-[5.5rem] md:px-[7rem] lg:px-[8rem]'>
+          <main className='flex w-[100vw] z-500 min-h-[550px] items-center mt-[10vh] justify-between sm:px-[5.5rem] md:px-[7rem] lg:px-[8rem]'>
             <div className=''>
-              <h1 className='tex font-pop'>THE PLACE<br />TO SHARE FUN<br />AND HUMOR</h1>
+              <h1 className='tex font-pop whitespace-nowrap'>THE PLACE<br />TO SHARE FUN<br />AND HUMOR</h1>
               <p className='font-pop font-normal text-lg w-[450px] mt-4 text-gray-200'>Bantr is a vibrant community to make new friends and connect with like-minded people. We like to spread positivity and fun by humor and laughter.</p>
               <button className='bg-[#FAC304] px-6 py-3 mt-5 rounded-full flex'><h1 className='text-black font-semibold text-xl'>Get Started</h1><i class="ri-arrow-right-line text-black text-2xl font-bold ml-2"></i></button>
             </div>
             <div className='monitor-container'>
               <div className='background'></div>
               {/* <Monitor /> */}
-              <h1 className='flex justify-center items-center h-[100%] opacity-70'>(removed monitor for performance reasons)</h1>
+              <h1 className='flex justify-center items-center h-[100%] opacity-70'>(removed monitor temporarily)</h1>
             </div>
           </main>
 
-          <div className='h-[100vh] bg-red-500'></div>
+          <div className='mt-10 mb-36 flex w-[100vw] z-500 justify-between sm:px-[5.5rem] md:px-[6rem] lg:px-[8rem]'>
+            <div className='w-[30%] flex justify-center mr-20'>
+              <img src="log.png" alt="" className='flex h-[12vw] ' />
+            </div>
+            <div className='w-[70%] max-w-[700px]'>
+              <h1 className='font-pop font-black text-6xl whitespace-nowrap'>WHAT IS BANTR?</h1>
+              <p className='font-pop font-normal text-md leading-[1.7rem] mt-4 w-[100%] max-w-[39rem] text-gray-200'>Bantr is a dynamic online community designed for teens to connect, share, and enjoy the lighter side of life. It's a place where humor thrives, friendships blossom, and creativity knows no bounds. Whether you're swapping memes, engaging in playful</p>
+              <div className='flex mt-2'>
+                <img src="star.png" alt="" className='h-8' />
+                <img src="star.png" alt="" className='h-8' />
+                <img src="star.png" alt="" className='h-8' />
+                <img src="star.png" alt="" className='h-8' />
+                <img src="star.png" alt="" className='h-8' />
+              </div>
+            </div>
+          </div>
+
+          <div className='mb-28'>
+            <h1 className='font-pop font-black text-6xl whitespace-nowrap text-center'>WHY BANTR?</h1>
+            <div className='flex justify-between sm:px-[5.5rem] md:px-[6rem] lg:px-[8rem] mt-14'>
+              <div className='w-1/3 flex flex-col items-center justify-center'>
+                <img src="/why/lock.png" alt="" className='h-[7vw]' />
+                <h1 className='font-pop font-semibold mb-1 mt-1 text-2xl whitespace-nowrap'>E2E Encryption</h1>
+                <p className='w-[70%] text-center text-gray-200'>At Bantr, your privacy and security are our top priorities. your messages and shared content are always protected</p>
+              </div>
+              <div className='w-1/3 flex flex-col items-center justify-center'>
+                <img src="/why/channel.png" alt="" className='h-[7vw]' />
+                <h1 className='font-pop font-semibold mb-1 text-2xl whitespace-nowrap'>Organized Channels</h1>
+                <p className='w-[70%] text-center text-gray-200'>We have channels for all topics.<br />Easily find and join channels that match your interests. With like minded people.</p>
+              </div>
+              <div className='w-1/3 flex flex-col items-center justify-center'>
+                <img src="/why/home.png" alt="" className='h-[7vw]' />
+                <h1 className='font-pop font-semibold translate-y-2 mb-2 mt-1 text-2xl whitespace-nowrap'>Best Community</h1>
+                <p className='w-[70%] text-center text-gray-200'>Meet like minded people at Bantr<br />Join a community of memers where friendships grow and every interaction is filled with smiles</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='mb-16 flex flex-col '>
+            <h1 className='font-pop font-black text-[3.5rem] leading-[1.13] whitespace-nowrap text-center'>WHAT ARE YOU WAITING FOR?<br /><span className='bg-gradient-to-r from-[#EEC800] to-[#FF8A00] text-transparent bg-clip-text'>JOIN US NOW!</span></h1>
+            <button className='bg-[#ffffff] m-auto px-7 py-3 mt-5 rounded-full flex'><h1 className='text-black font-semibold text-xl'>Get Started</h1><i class="ri-arrow-right-line text-black text-2xl font-bold ml-2"></i></button>
+          </div>
+
+          <div className='mb-72'>
+            <h1 className='text-center text-[16vw] tracking-wider font-pop leading-none opacity-25 bg-gradient-to-b from-[#FFFFFF] to-[#414141] text-transparent bg-clip-text'>Contact Us</h1>
+            <div className='sm:px-[5.5rem] md:px-[7rem] lg:px-[8rem] -translate-y-14'>
+              <div className='flex flex-col justify-between'>
+                <div>
+                  <h1 className='leading-none font-pop font-bold text-[5vw] text-gray-100'>Reach Out <i class="ri-arrow-right-up-line bg-gradient-to-bl text-8xl from-[#FFFFFF] to-[#414141] text-transparent bg-clip-text"></i></h1>
+                  <p className='mt-3 text-[1.2vw] w-[35vw] text-[#d2d2d2] leading-[1.2] opacity-90'>Have a question or need assistance? Whether you're seeking support, have an inquiry, or simply want to connect, feel free to reach out. I'm always here to help and will make every effort to respond to you. I look forward to connecting with you soon.</p>
+                  <div className='mt-10'>
+                    <div className='flex items-center mb-4'>
+                      <div className='flex w-[2.7vw] h-[2.7vw] bg-[#DEDEDE] mr-5 rounded-full justify-center items-center'>
+                        <img src="check.png" alt="" className='' />
+                      </div>
+                      <h1 className='font-pop font-medium text-lg'>Timely Response</h1>
+                    </div>
+                    <div className='flex items-center'>
+                      <div className='flex w-[2.7vw] h-[2.7vw] bg-[#DEDEDE] mr-5 rounded-full justify-center items-center'>
+                        <img src="check.png" alt="" className='w-' />
+                      </div>
+                      <h1 className='font-pop font-medium text-lg'>Personalized response</h1>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            
+            </div>
+          </div>
         </div>
       </div>
     </>
