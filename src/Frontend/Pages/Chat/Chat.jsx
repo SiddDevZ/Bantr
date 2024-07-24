2bc472d0b6303d2781bec1cafb4c24638e357403
SiddDevZ <epic.siddarth@gmail.com>
2024-07-25 04:39:32 +0530

    Trying to fix v4

--- src/Frontend/Pages/Chat/Chat.jsx
+++ src/Frontend/Pages/Chat/Chat.jsx
@@ -170,7 +170,6 @@ const Chat = () => {
 
   useEffect(() => {
     const newSocket = io(config.normal, {
-      transports: ['websocket'],
       upgrade: false,
       forceNew: true,
       reconnection: true,
